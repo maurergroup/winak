5503ae77c2f9562152dcf08ace7af6f95c352850
Konstantin Krautgasser <krautgasser@theo.chemie.tu-muenchen.de>
2014-06-25 17:30:24 +0200

    Added QD folder

---
+++ QD/__init__.py
@@ -0,0 +1,25 @@
+# thctk.QD initialization
+#
+#
+#   thctk - python package for Theoretical Chemistry
+#   Copyright (C) 2002 Christoph Scheurer
+#
+#   This file is part of thctk.
+#
+#   thctk is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   thctk is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+Quantum Dynamics
+"""

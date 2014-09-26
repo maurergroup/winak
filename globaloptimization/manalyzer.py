cd4b6bc28b6d3435911a822b3ccfc7bf566bcf1e
konstantin <konstantin@kkws.(none)>
2014-09-26 13:32:53 +0200

    > reintroduced adsorbate handling for Cartesian displacements > added example for Cartesian adsorbate handling (tests/surface) > added globaloptimization/manalyzer.py (Minima Analyzer)   - sorts minima by RMSD to internal coordinates, Energy and     number of primitive internal coordinates generated by VCG > bugfixes in disspotter.py

---
+++ globaloptimization/manalyzer.py
@@ -0,0 +1,122 @@
+from INTERNALS.curvilinear.InternalCoordinates import ValenceCoordinateGenerator as VCG
+from INTERNALS.globaloptimization.disspotter import *
+from ase.io.trajectory import PickleTrajectory
+import numpy as np
+from hotbit import *
+from hotbit.coulomb import MultipoleExpansion
+
+class Manalyzer:
+    """
+    uses RMSD to determine which minma are the same.
+    """
+    def __init__(self,trajectory,rmsd=-1,lenthr=-1,edif=-1):
+        """
+        trajectory can either be a string or a ase.io.trajectory PickleTrajectory type
+        """
+        if isinstance(trajectory,basestring):
+            self.allmin=PickleTrajectory(trajectory,'r')
+        else:
+            self.allmin=trajectory
+        if rmsd==-1:
+            self.rmsd=1.2
+        else:
+            self.rmsd=rmsd
+        if lenthr==-1:
+            self.lent=2
+        else:
+            self.lent=lenthr#
+        if edif==-1:
+            self.edif=0.25
+        else:
+            self.edif=edif
+        self.notdis=[]
+        self.dis=[]
+        self.min=[]
+
+    def dothebartman(self):
+        tmp=0
+
+
+        d = '/home/konstantin/software/HOTBIT/param/SURF/'
+
+        elm = {'H': d + 'H.elm',
+               'C': d + 'C.elm',
+               'N': d + 'N.elm',
+               'O': d + 'O.elm'}
+
+        tab = {'CH': d + 'C_H.par',
+               'HH': d + 'H_H.par',
+               'CC': d + 'C_C.par',
+               'CN': d + 'C_N.par',
+               'NH': d + 'N_H.par',
+               'NN': d + 'N_N.par',
+               'rest': 'default'}
+
+        mixer = {'name': 'Pulay', 'convergence': 1E-7}
+        calc_dftb = Hotbit(txt='hotbit.txt',
+                   elements=elm,
+                   mixer=mixer,
+                   tables=tab,
+                   SCC=True,
+                   verbose_SCC=True,
+                   width=0.10,
+                   maxiter=100,
+                   coulomb_solver=MultipoleExpansion(
+                       n=(3, 3, 1),
+                       k=(1, 1, 1)),
+                   kpts=(1,1,1)
+                   )
+
+
+        for i in self.allmin:
+            e=DisSpotter(atoms=i)
+            if e.spot_dis():
+                self.dis.append(Minimum(i,e.vcg,e.iclist,e.ic,tmp))
+            else:
+                i.set_calculator(calc_dftb)
+                print 'lol'
+                self.notdis.append(Minimum(i,e.vcg,e.iclist,e.ic,tmp,i.get_potential_energy()))
+                tmp+=1
+        for i in self.notdis:
+            if len(self.min)==0:
+                tmpmin=[]
+                tmpmin.append(i)
+                self.min.append(tmpmin)
+            else:
+                found=False
+                for j in self.min:
+                    tmpic=icSystem(j[0].iclist,len(j[0].atoms),masses=j[0].atoms.get_masses(),xyz=i.atoms.get_positions().flatten())
+                    cic=tmpic()
+                    tmpsum=0
+                    for k in range(len(cic)):
+                        tmpsum+=(cic[k]-j[0].cic[k])**2
+                    rmsd=np.sqrt(tmpsum/len(cic))
+                    print 'rmsd from '+str(i.oldn)+ ' to '+str(j[0].oldn)+' = '+str(rmsd)+' ,dif= '+str(np.abs(len(i.cic)-len(j[0].cic)))+' , Edif = '+str(np.abs(i.E-j[0].E))
+                    if rmsd<self.rmsd and np.abs(len(i.cic)-len(j[0].cic))<self.lent and np.abs(i.E-j[0].E)<self.edif:
+                        j.append(i)
+                        found=True
+                        break
+                if not found:
+                    tmpmin=[]
+                    tmpmin.append(i)
+                    self.min.append(tmpmin)
+
+        for i in range(len(self.min)):
+            xx=PickleTrajectory(str(i)+'.traj','w')
+            print 'minim: '+str(i)
+            for j in range(len(self.min[i])):
+                xx.write(self.min[i][j].atoms)
+                print self.min[i][j].oldn
+
+class Minimum:
+    """
+    convenience class, used to avoid a million lists
+    """
+    def __init__(self,atoms,vcg,icl,ic,oldnum,en=None):
+        self.atoms=atoms
+        self.vcg=vcg
+        self.iclist=icl
+        self.ic=ic
+        self.oldn=oldnum
+        self.cic=ic()
+        self.E=en
---
+++ tests/surface/surface_test_DI.py
@@ -0,0 +1,65 @@
+#from ase.lattice.surface import fcc111, add_adsorbate
+#from ase.calculators.dftdisp import dftdisp
+from ase import Atoms
+from ase.calculators.dftdisp import dftdisp
+from ase.all import *
+from ase.calculators.dftb import Dftb
+
+from ase.calculators.qmme import qmme
+from ase.optimize import BFGS
+from INTERNALS.globaloptimization.betterhopping import BetterHopping
+import numpy as np
+
+adsorbate = '../testsystems/NH3_on_Ag100.traj'
+d='/home/konstantin/software/DFTB/param/SURF/'
+molecule = read(adsorbate)
+
+mixer = {'name': 'Pulay', 'convergence': 1E-7}
+
+slab=molecule[0:8]
+
+calc_dftb = Dftb(label='dftb',atoms=molecule,kpts=(2,2,1),
+                 Hamiltonian_MaxAngularMomentum_ = '',
+                 Hamiltonian_MaxAngularMomentum_N = '"p"',
+                 Hamiltonian_MaxAngularMomentum_H = '"s"',
+                 Hamiltonian_MaxAngularMomentum_Ag = '"d"',
+                 Hamiltonian_MaxSCCIterations = 2000,
+                 Hamiltonian_SCC ='YES',
+                 Hamiltonian_SCCTolerance = 1.0E-007,
+                 Hamiltonian_SlaterKosterFiles_Prefix=d,
+                 )
+
+calc_vdw = dftdisp(atoms=None,
+                   sedc_print_level=5,
+                   sedc_pbc_g_only_intra=[0, -1],
+                   sedc_scheme='TS-SURF',
+                   sedc_n_groups=2,
+                   sedc_groups=[8,3],
+                   sedc_pbc_g_switches=[[0, 0, 0, 0, 0, 0],
+                                        [1, 1, 0, 1, 1, 0]],
+                   sedc_tssurf_vfree_div_vbulk=[1.00, 1.00, 1.00],
+                   sedc_do_standalone=False)
+
+QMMM = qmme(atoms=molecule,
+            nqm_regions=1,
+            nmm_regions=1,
+            qm_calculators=[calc_dftb],
+            mm_calculators=[calc_vdw],
+            qm_atoms=[[(4,12)]],
+            mm_mode='allatoms')
+
+molecule.set_calculator(QMMM)
+
+bh = BetterHopping(atoms=molecule,
+                  temperature=100 * kB,
+                  dr=1.0,
+                  optimizer=BFGS,
+                  fmax=2,
+                  logfile='tt.log',
+                  maxmoves=50,
+                  movemode=1,
+                  numdelocmodes=3,
+                  adsorbmask=(8,12),
+                  cell_scale=(0.5,0.5,0.1)
+                  )
+bh.run(20)

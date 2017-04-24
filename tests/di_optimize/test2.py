a6b23b37eaba1c8d6d899b8e8aa2f198925379ee
Reinhard Maurer <reinhard.maurer@yale.edu>
2017-04-24 10:43:14 -0400

    current non-working state of DIopt

--- tests/di_optimize/test2.py
+++ tests/di_optimize/test2.py
@@ -4,28 +4,37 @@ from ase.visualize import view
 import numpy as np
 
 from ase.structure import molecule
-from obcalc import OBForceField
+#from obcalc import OBForceField
+from ase.calculators.emt import EMT
 from ase.optimize import BFGS
 
 from winak.optimize import DIopt
 
-atoms =read('../testsystems/clethen.xyz')
+atoms =read('../testsystems/Cucluster.xyz')
+#atoms =read('../testsystems/peptide.xyz')
+atoms.rattle()
 
-calc = OBForceField()
+calc = EMT()
 atoms.set_calculator(calc)
 e = atoms.get_potential_energy()
 
 dyn = BFGS(atoms)
-dyn.run(0.01)
+dyn.run(0.1)
 
-view(atoms)
+# view(atoms)
 
 raw_input('press enter!')
 
-atoms =read('../testsystems/clethen.xyz')
+atoms =read('../testsystems/Cucluster.xyz')
+#atoms =read('../testsystems/clethen.xyz')
+atoms.rattle()
+
+#calc = OBForceField()
+calc = EMT()
+atoms.set_calculator(calc)
 
 dyn = DIopt(atoms)
-dyn.run(0.01)
+dyn.run(0.1)
 
 view(atoms)
 

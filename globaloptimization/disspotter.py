50299b4ec2ed7ea57ec9604ed92cd0efc9e670e4
Reinhard Maurer <reinhard.maurer@yale.edu>
2017-04-24 10:37:06 -0400

    compatibility with newer ASE (ase.all does not exist anymore)

--- globaloptimization/disspotter.py
+++ globaloptimization/disspotter.py
@@ -18,7 +18,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>#
 
-from ase.all import *
 from winak.curvilinear.InternalCoordinates import icSystem
 from winak.curvilinear.InternalCoordinates import ValenceCoordinateGenerator as VCG
 from winak.curvilinear.Coordinates import InternalCoordinates as IC
